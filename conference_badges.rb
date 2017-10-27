e5f7fbcd0d0a66d4d392e13dbe2e08e89acc3d47
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-27 20:13:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker(name) 
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  greetings = Array.new(names.size)
+  counter = 0
+  
+  names.each do |name|
+    greetings[counter] = "Hello, my name is #{name}."
+    coutner += 1
+  end
+
+  return greetings
+end
+
